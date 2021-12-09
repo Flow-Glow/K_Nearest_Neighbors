4d4e61e9e2e0dafc693001dbae3be247f1a68248
Wolf <89105476+WolfPack9108@users.noreply.github.com>
2021-12-08 17:00:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import mode
-import TrainingData as td
+import TrainingData as tds
 from sklearn.datasets import make_blobs
 
+fig = plt.figure()
+ax = fig.add_subplot()
 
-Default_data, label, centroids = make_blobs(n_samples=100, centers=2, n_features=2, random_state=0,
-                                            return_centers=True, cluster_std=1)
+Default_data, label, centroids = make_blobs(n_samples=1000, centers=2, n_features=2,
+                                            return_centers=True, cluster_std=10,)
 
-data = np.linspace((1, 1), (10, 10), 10)
-
-
-
-
-def euclidean_distance_calc(a, b):
-    distance = 0
-    for i in range(len(a)):
-        bma = (a[i] - b[i])
-        bmas = np.square(bma)
-        distance = np.sum(bmas) + distance
-    return np.sqrt(distance)
-
-
-def scatter_plot(td, new_points,mode):
+def scatter_plot(td, new_points, point, regression=False, classification=False):
     clr = np.array(["red", "blue"])
+    training_data = tds.training_data_generator(Default_data, centroids)
+    for i in training_data:
+        training_data[i] = np.array(training_data[i])
 
-    for i in td.keys():
-        td[i] = np.array(td[i])
+        ax.set_ylabel("Y axis")
+        ax.set_xlabel("X axis")
+        # ax.set_zlabel("Z axis")
+        if classification:
 
-        plt.xlabel("X axis")
-        plt.ylabel("Y axis")
-        plt.scatter(td[i][:, 0], td[i][:, 1], c=clr[i], alpha=0.5, s=10)
-        plt.scatter(new_points[0], new_points[1], c=clr[mode], marker="x", s=(5 * 5) ** 2)  # c=clr[i]
+            ax.scatter(training_data[i][:, 0], training_data[i][:, 1], c=clr[i], alpha=0.5, s=10)  # td[i][:, 2]
+            ax.scatter(new_points[0], new_points[1], c=clr[point], marker="x", s=(5 * 5) ** 2)  # c=clr[i] new_points[2]
+    if regression:
+        ax.scatter(td[:, 0], td[:, 1], c="g", alpha=0.03, s=10)  # td[i][:, 2]
+        ax.scatter(new_points[0], new_points[1], c="black", marker="x", s=(5 * 5) ** 2)  # new_points[2],
+        ax.scatter(point[0], point[1], color="y", marker="x", s=(5 * 5) ** 2)  # point[2]
 
-def classify_point(point, td, k):
+
+def classify_point(point, td, k, classification=False, regression=False):
     euclidean = []
     labels = []
-    for i in td:
-        for j in td[i]:
-            Euclidean = euclidean_distance_calc(point, j)
+
+    if classification:
+        training_data = tds.training_data_generator(Default_data, centroids)
+        for i in training_data:
+            for j in training_data[i]:
+                Euclidean = tds.euclidean_distance_calc(point, j)
+                euclidean.append(Euclidean)
+                labels.append(i)
+        s_euclidean = np.array(euclidean)
+        s_euclidean = np.argsort(s_euclidean)
+        s_labels = np.array(labels)
+        s_labels = s_labels[s_euclidean]
+        labels = s_labels[:k]
+        return mode(labels)[0]
+    if regression:
+      
+        for i in td:
+            Euclidean = tds.euclidean_distance_calc(point, i)
             euclidean.append(Euclidean)
-            labels.append(i)
-    s_euclidean = np.array(euclidean)
-    s_euclidean = np.argsort(s_euclidean)
-    s_labels = np.array(labels)
-    s_labels = s_labels[s_euclidean]
-    labels = s_labels[:k]
-    return mode(labels)[0]
+        s_euclidean = np.array(euclidean)
+        s_euclidean = np.argsort(s_euclidean)
+        s_td = np.array(td)
+        s_td = s_td[s_euclidean]
+        closest = s_td[:k]
+        print(closest)
+        return np.mean(closest, axis=0)
+
 
 def main():
-    new_point = np.array([[2, 0],[-1, 1],[5, 10],[3, 4]])
-    training_data = td.training_data_generator(Default_data, centroids)
-    for p in new_point:
-        mode = classify_point(point=p, td=training_data, k=2)
-        scatter_plot(training_data, p, mode)
+    new_point = (np.random.random([10,2]) * 20)-10
+    for test_point in new_point:
+        point = classify_point(point = test_point, td = Default_data, k=3, classification=True)
+        scatter_plot(Default_data, test_point, point, classification=True)
+
 
 if __name__ == '__main__':
     main()
-
-plt.grid()
+plt.grid(True)
 plt.show()
-
